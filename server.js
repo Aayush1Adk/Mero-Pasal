973fffac3a7cd4869d0894acd5d90372979a95ac
Aayush Adhikari <aayush.adhikari2076@gmail.com>
2026-04-25 13:50:07 +0545

    retry

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./Backend/src/app.js");
-const ConnectDB = require("./Backend/src/db/db.js")
-
-ConnectDB();
-
-app.listen(3000, ()=>{
-    console.log("Server is running in Port 3000 ")
-});
